e71530700b3129908605017b7f274d7c96beb3d1
Patryk Klimas <patryk.bajos@gmail.com>
2019-01-01 14:28:36 +0100

    Created base class for toolchains, fixed schema dict

--- buildz/toolchain/generic.py
+++ buildz/toolchain/generic.py
@@ -1,25 +1,41 @@
 from schema import Schema, SchemaError
 
+
 class GenericToolchain():
     _env_sch_val = {}
     _conf_sch_val = {}
 
-    def _set_env(self, env):
+    def __init__(self, conf, env):
+        self.__env = {}
+        self.__conf = {}
+
+        self.env = env
+        self.conf = conf
+
+    @property
+    def env(self):
+        return self.__env
+
+    @env.setter
+    def env(self, env):
         try:
             env_sch = Schema(self._env_sch_val)
-            self._env = env_sch.validate(env)
+            self.__env = env_sch.validate(env)
         except SchemaError as e:
-            print('AbstractToolchain._set_env(): Error validating env.\n', e)
-            self._env = {}
-            raise ValueError("AbstractToolchain._set_env(): Invalid env.")
-        return
+            print('AbstractToolchain.env(): Error validating env.\n', e)
+            self.__env = {}
+            raise ValueError("AbstractToolchain.env(): Invalid env.")
 
-    def _set_conf(self, conf):
+    @property
+    def conf(self):
+        return self.__conf
+
+    @conf.setter
+    def conf(self, conf):
         try:
             conf_sch = Schema(self._conf_sch_val)
-            self._conf = conf_sch.validate(conf)
+            self.__conf = conf_sch.validate(conf)
         except SchemaError as e:
-            print('AbstractToolchain._set_conf(): Error setting conf.\n', e)
-            self._conf = {}
-            raise ValueError('AbstractToolchain._set_conf: Invalid conf.')
-        return
+            print('AbstractToolchain.conf(): Error setting conf.\n', e)
+            self.__conf = {}
+            raise ValueError('AbstractToolchain.conf: Invalid conf.')

f1bdb62d5b39d01c42d3181d656898908263fb66
Patryk Klimas <patryk.bajos@gmail.com>
2019-01-01 14:33:45 +0100

    Added runpath, changed method names (according to runpath)

--- buildz/frontend/vscode.py
+++ buildz/frontend/vscode.py
@@ -1,28 +1,31 @@
-from buildz.utils import get_dicts_with_value, get_buildz_conf, nodupl_append_dict_to_list
-from buildz.toolchain.avrgcc import AvrGccToolchain
-import platform
 import json
+import platform
+
+from buildz.toolchain.factory import factory_named_toolchain
+from buildz.utils import (get_buildz_conf, get_dicts_with_value,
+                          nodupl_append_dict_to_list)
+
 
 class VSCodeFrontend():
-    vsc_sys_name_dict = {
+    __vsc_sys_name_dict = {
         'Linux': 'Linux',
         'Darwin': 'Mac',
         'Windows': 'Win32'
     }
 
-    tasks_file_path = '.vscode/tasks.json'
-    cpp_prop_file_path = '.vscode/c_cpp_properies.json'
+    tasks_path = '.vscode/tasks.json'
+    cpp_prop_path = '.vscode/c_cpp_properies.json'
 
     def __init__(self):
-        tasks_file = open(self.tasks_file_path, 'r')
-        cpp_prop_file = open(self.cpp_prop_file_path, 'r')
+        tasks_file = open(self.tasks_path, 'r')
+        cpp_prop_file = open(self.cpp_prop_path, 'r')
 
         local_sys_name = platform.system()
-        if not local_sys_name in self.vsc_sys_name_dict:
+        if not local_sys_name in self.__vsc_sys_name_dict:
             print('Unsupported platform system: {}'.format(local_sys_name))
             exit
 
-        self.vsc_sys_name = self.vsc_sys_name_dict[local_sys_name]
+        self.vsc_sys_name = self.__vsc_sys_name_dict[local_sys_name]
         self.cpp_prop_conf_name = self.vsc_sys_name
         self.tasks_json = json.load(tasks_file)
         self.cpp_prop_json = json.load(cpp_prop_file)
@@ -32,12 +35,11 @@ class VSCodeFrontend():
         return
 
     def save_tasks_file(self):
-        tasks_file = open(self.tasks_file_path, 'w')
+        tasks_file = open(self.tasks_path, 'w')
         json.dump(self.tasks_json, tasks_file, indent=4)
         tasks_file.close()
-        return
 
-    def update_tasks(self):
+    def gen_tasks(self):
         try:
             build_conf = get_buildz_conf()
         except Exception as exc:
@@ -47,41 +49,32 @@ class VSCodeFrontend():
         targets = build_conf['targets']
         toolchains = build_conf['toolchains']
 
-        for target_name, target in targets.items():
-            toolchain_name = target['toolchain']
-            toolchain = toolchains.get(toolchain_name, None)
-
-            if not toolchain:
-                print('VSCodeFrontend.update_tasks(): Target {} uses toolchain {} wich does not exist.')
-                break
-            
-            # toolchain is now safe structure
-
-            toolchain_type = toolchain['type']
-            toolchain_handler = None
-
-            if toolchain_type == 'gcc':
-                toolchain_handler = GccToolchain(toolchain['env'])
-            if toolchain_type == 'avr-gcc':
-                toolchain_handler = AvrGccToolchain(toolchain['env'])
+        for trg_name, target in targets.items():
+            tch_name = target['toolchain']
+            tch_handle = factory_named_toolchain(tch_name, toolchains)
 
             tasks = self.tasks_json['tasks']
-            tch_tasks = toolchain_handler.get_select_target_task()
 
-            for fcpu in fcpus:
-                task = {}
-                label = 'select {} {}'.format(mcu, fcpu)
-                cmd = "python3 -m buildz vscode updatetasks \"{}\" {} {}".format(path, mcu, fcpu)
-                task["type"] = "shell"
-                task["label"] = label
-                task["command"] = cmd
-                nodupl_append_dict_to_list(tasks, "label", task)
+            #TODO update tasks
 
         self.save_tasks_file()
         return
 
-    def select_target(self):
+    def select_target(self, trg_name):
+        conf_base = self.cpp_prop_json
+
+        env = conf_base['env']
+        confs = conf_base['configurations']
+
         return
 
-    def run(self, mod_args):
-        return
+    _route = {
+        'update': {
+            'tasks': gen_tasks
+        },
+        'select': {
+            'target': select_target
+        }
+    }
+
+

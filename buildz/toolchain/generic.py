38883760fe7070fab12b0d4759444e48f463b099
Patryk Klimas <patryk.bajos@gmail.com>
2019-01-27 13:11:03 +0100

    Fitted to new config

--- buildz/toolchain/generic.py
+++ buildz/toolchain/generic.py
@@ -2,40 +2,42 @@ from schema import Schema, SchemaError
 
 
 class GenericToolchain():
-    _env_sch_val = {}
-    _conf_sch_val = {}
+    _envsch = Schema({})
+    _confsch = Schema({})
 
-    def __init__(self, conf, env):
-        self.__env = {}
-        self.__conf = {}
+    def __init__(self, tchsetup):
+        self.__setup = tchsetup
+        self.__valid = self.__validate_setup()
 
-        self.env = env
-        self.conf = conf
+    def __validate_setup(self):
+        try:
+            self.__setup.env = self._envsch.validate(self.__setup.env)
+        except SchemaError as e:
+            print('GenericToolchain(): Error validating env.\n', e)
+            raise ValueError("AbstractToolchain: Invalid env.")
+
+        try:
+            self.__setup.conf = self._confsch.validate(self.__setup.conf)
+        except SchemaError as e:
+            print('GenericToolchain(): Error validating conf.\n', e)
+            raise ValueError('AbstractToolchain.conf: Invalid conf.')
+
+        return True
+
+    def build_mod(self, config, toolchain, module, target):
+        pass
+
+    def is_valid(self):
+        return self.__valid
+
+    @property
+    def setup(self):
+        return self.__setup
 
     @property
     def env(self):
-        return self.__env
-
-    @env.setter
-    def env(self, env):
-        try:
-            env_sch = Schema(self._env_sch_val)
-            self.__env = env_sch.validate(env)
-        except SchemaError as e:
-            print('AbstractToolchain.env(): Error validating env.\n', e)
-            self.__env = {}
-            raise ValueError("AbstractToolchain.env(): Invalid env.")
+        return self.__setup.env
 
     @property
     def conf(self):
-        return self.__conf
-
-    @conf.setter
-    def conf(self, conf):
-        try:
-            conf_sch = Schema(self._conf_sch_val)
-            self.__conf = conf_sch.validate(conf)
-        except SchemaError as e:
-            print('AbstractToolchain.conf(): Error setting conf.\n', e)
-            self.__conf = {}
-            raise ValueError('AbstractToolchain.conf: Invalid conf.')
+        return self.__setup.conf

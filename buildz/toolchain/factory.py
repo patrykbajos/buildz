d477364bee2466200230a9a06b005411cf82f76d
Patryk Klimas <patryk.bajos@gmail.com>
2019-01-27 13:12:00 +0100

    Fitted to new config classes

--- buildz/toolchain/factory.py
+++ buildz/toolchain/factory.py
@@ -1,20 +1,11 @@
 from buildz.toolchain.avrgcc import AvrGccToolchain
 from buildz.toolchain.gcc import GccToolchain
 
-def factory_named_toolchain(tch_name, tch_dict):
-    tch = tch_dict.get(tch_name)
-
-    if tch is None:
-        raise KeyError('Missing toolchain {} in toolchains.'.format(tch_name))
-
-    temp_tch = factory_toolchain_type(tch['type'], tch['conf'], tch['env'])
-
-    return temp_tch
-
-def factory_toolchain_type(type_name, conf, env):
-    if type_name == 'GccToolchain':
-        return GccToolchain(conf, env)
-    if type_name == 'AvrGccToolchain':
-        return AvrGccToolchain(conf, env)
+def factory_toolchain(tch):
+    if tch.type == 'GccToolchain':
+        return GccToolchain(tch)
+    if tch.type == 'AvrGccToolchain':
+        return AvrGccToolchain(tch)
     
-    raise ValueError('Unknown toolchain type {}.'.format(type_name))
+    raise ValueError('Unknown toolchain type {}.'.format(tch.type))
+    

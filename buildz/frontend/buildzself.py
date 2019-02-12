1db34d44eb245befacec7d3e443f7da48c67aa6b
Patryk Klimas <patryk.bajos@gmail.com>
2019-02-12 22:58:44 +0100

    Fitted for new config classes

--- buildz/frontend/buildzself.py
+++ buildz/frontend/buildzself.py
@@ -1,35 +1,29 @@
-from buildz.utils import get_buildz_conf
-from buildz.toolchain.factory import factory_named_toolchain
+from buildz.config.bzconfig import BZConfig
+from buildz.config.module import Module
+from buildz.toolchain.factory import factory_toolchain
 
 class BuildzSelfFrontend():
     def __init__(self):
         return
 
     def build(self):
-        bz_conf = get_buildz_conf()
+        bz = BZConfig.from_file()
 
-        tchs = bz_conf['toolchains']
-        trgs = bz_conf['targets']
+        for trg in bz.targets:
+            print('Processing target {}.'.format(trg.name))
 
-        build_type = bz_conf['build']['type']
+            # trg.modules can be mod names list or string 'all'
+            if trg.modules == 'all':
+                modules = bz.modules
+            else:
+                modules = trg.modules
 
-        for trg_name, trg in trgs.items():
-            print('Processing target {}.'.format(trg_name))
+            tch = bz.toolchains['trg.toolchain']
+            tch_handler = factory_toolchain(tch)
 
-            # tg.modules can be mod names list or string 'all'
-            trg_mod_names = trg['modules']
-            if trg_mod_names == 'all':
-                trg_mod_names = bz_conf['modules']
-
-            trg_env = trg['env']
-
-            tch_name = trg['toolchain']
-            tch = tchs[tch_name]
-            tch_handler = factory_named_toolchain(tch_name, tchs)
-
-            for mod_name in trg_mod_names:
-                print('Processing module {} of target {}.'.format(mod_name, trg_name))
-            
-                tch_handler.build_mod(build_type, mod_name, tch_name, tch, trg_name, trg_env)
+            for mod_name in modules:
+                print('Processing module {} of target {}.'.format(mod_name, trg.name))
+                mod = Module.from_file(mod_name)
+                tch_handler.build_mod(bz, mod, trg)
 
     _route = build
